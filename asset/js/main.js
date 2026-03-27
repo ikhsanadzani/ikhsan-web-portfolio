aa06609833908f426f28649afafea24910ee3272
ikhsanadzani <ikhsanadzni@gmail.com>
2026-03-28 00:30:53 +0700

    update

--- asset/js/main.js
+++ asset/js/main.js
@@ -94,18 +94,71 @@ servicesButtons.forEach(button => {
 
 
 /*=============== COPY EMAIL IN CONTACT ===============*/
+const copyBtn = document.getElementById('contact-btn'),
+      copyEmail = document.getElementById('contact-email').textContent
 
+copyBtn.addEventListener('click', () => {
+  navigator.clipboard.writeText(copyEmail).then(() => {
+    copyBtn.innerHTML = 'Email copied <i class="ri-check-line"></i>'
+
+    setTimeout(() => {
+      copyBtn.innerHTML = 'Copy email <i class="ri-file-copy-line"></i>'
+    }, 2000)
+  })
+})
 
 /*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
+const textYear = document.getElementById('footer__year')
+      currentYear = new Date().getFullYear()
 
+textYear.textContent = currentYear
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const section = document.querySelectorAll('section[id]')
 
+const scrollActive = () => {
+  const scrollY = window.scrollY
+
+  section.forEach (section => {
+    const id = section.id,
+          top = section.offsetTop - 50,
+          height = section.offsetHeight,
+          link = document.querySelector('.nav-menu a[href*=' + id + ']')
+    if(!link) return
+
+    link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+  })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== CUSTOM CURSOR ===============*/
+const cursor = document.querySelector('.cursor')
+let mouseX = 0, mouseY = 0
 
+const cursorMove = () => {
+  cursor.style.left = `${mouseX}px`
+  cursor.style.top = `${mouseY}px`
+  cursor.style.transform = 'translate(-50%, -50%)'
+
+  requestAnimationFrame(cursorMove)
+}
+
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX
+  mouseY = e.clientY
+})
+
+cursorMove()
 
 /* Hide custom cursor on links */
+const a = document.querySelectorAll('a')
 
-
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hide-cursor')
+  })
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hide-cursor')
+  })
+});
 /*=============== SCROLL REVEAL ANIMATION ===============*/
